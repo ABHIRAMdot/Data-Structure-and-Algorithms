b0e0bade7d08c8c382aea9701b75468c1f802e31
Abhiram P V <abhirampv100@gmail.com>
2026-02-24 08:25:18 +0530

    DSA-2 Stack

--- DSA 2/stack.py
+++ DSA 2/stack.py
@@ -1,48 +1,130 @@
-# s = []
-# s.append('https://www.cnn.com/')
-# s.append('https://www.cnn.com/world')
-# s.append('https://www.cnn.com/india')
-# s.append('https://www.cnn.com/china')
+# Stack using Array
 
+# class Stack:
+#     def __init__(self, size):
+#         self.size = size
+#         self.arr = [None] * size
+#         self.top = -1        #stack pointer
+
+#     def isEmpty(self):
+#         return self.top == -1
+    
+#     def isFull(self):
+#         return self.top == self.size -1
+    
+#     def push(self, value):
+#         if self.isFull():
+#             print("Stack Overflow")
+#             return
+#         self.top += 1
+#         self.arr[self.top] = value
+
+#     def pop(self):
+#         if self.isEmpty():
+#             print("Stack underflow")
+#             return 
+#         value = self.arr[self.top]
+#         self.top -= 1
+#         return value
+    
+#     def peek(self):
+#         if self.isEmpty():
+#             print("Stack is Empty")
+#             return
+#         return self.arr[self.top]
+    
+
+# s = Stack(3)
+# s.push(10)
+# s.push(20)
+# s.push(30)
+# # s.pop()
+# print(s.peek())
+
+
+# class Stack:
+#     def __init__(self, size):
+#         self.size = size
+#         self.arr = [None] * size
+#         self.top = -1
+
+#     def isEmpty(self):
+#         return self.top == -1
+    
+#     def isFull(self):
+#         return self.top == self.size -1
+    
+#     def push(self, value):
+#         if self.isFull():
+#             print("Stack Overflow")
+#             return
+#         self.top += 1
+#         self.arr[self.top] = value
+        
+    
+#     def pop(self):
+#         if self.isEmpty():
+#             print("Stack underflow")
+#             return 
+#         value = self.arr[self.top]
+#         self.top -= 1
+#         return value
+    
+#     def peek(self):
+#         if self.isEmpty():
+#             print("Stack is Empty")
+#             return
+#         return self.arr[self.top]
+    
+# s = Stack(3)
+
+# s.push(2)
+# s.push(3)
+# s.push(4)
 # print(s.pop())
+# print(s.peek())
+
+
+
+# Stack using Linked List
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
 
 class Stack:
-    def __init__(self, size):
-        self.size = size
-        self.arr = [None] * size
-        self.top = -1        #stack pointer
-
+    def __init__(self):
+        self.top = None
+    
     def isEmpty(self):
-        return self.top == -1
+        return self.top is None
     
-    def isFull(self):
-        return self.top == self.size -1
-    
-    def push(self, value):
-        if self.isFull():
-            print("Stack Overflow")
-            return
-        self.top += 1
-        self.arr[self.top] = value
+    def push(self, val):
+        new_node = Node(val)
+        new_node.next = self.top
+        self.top = new_node
 
     def pop(self):
         if self.isEmpty():
-            print("Stack underflow")
-            return 
-        value = self.arr[self.top]
-        self.top -= 1
+            print("Stack Underflow")
+            return
+        value = self.top.data
+        self.top = self.top.next
         return value
     
     def peek(self):
         if self.isEmpty():
             print("Stack is Empty")
             return
-        return self.arr[self.top]
+        
+        return self.top.data
     
+s = Stack()
+s.push(222)
+s.push(444)
+s.push(555)
 
-s = Stack(3)
-s.push(10)
-s.push(20)
-s.push(30)
-# s.pop()
+print(s.pop())
 print(s.peek())
