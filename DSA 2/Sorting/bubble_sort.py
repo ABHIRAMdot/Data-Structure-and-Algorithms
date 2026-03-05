8bd4889798bd62724f24b31caf4c4e552c3782f2
Abhiram P V <abhirampv100@gmail.com>
2026-03-05 09:24:08 +0530

    DSA-2

--- DSA 2/Sorting/bubble_sort.py
+++ DSA 2/Sorting/bubble_sort.py
@@ -16,4 +16,23 @@ def bubble_sort(arr):
 
 num = [5, 3, 8, 2]
 
-print(bubble_sort(num))
+# print(bubble_sort(num))
+
+
+# def bubble_sort(arr):
+#     n = len(arr)
+
+#     for i in range(n):
+#         swapped = False
+
+#         for j in range(0, n - i- 1):
+#             if arr[j]  > arr[j+1]:
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+#                 swapped = True
+
+#         if not swapped:
+#             break
+
+#     return arr
+
+# print(bubble_sort(num))
