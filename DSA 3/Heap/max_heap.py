161ca772dcb635be11b1a98f5d9765388fccdc98
Abhiram P V <abhirampv100@gmail.com>
2026-03-14 09:31:00 +0530

    DSA-3

---
+++ DSA 3/Heap/max_heap.py
@@ -0,0 +1,60 @@
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    # Insert element
+    def insert(self, val):
+        self.heap.append(val)
+        self.heapify_up(len(self.heap)-1)
+
+    #heapify upward
+    def heapify_up(self, index):   #index will be the next position that we are inserting in the heap
+        parent = (index - 1) // 2  # here parent is the root of the new node 
+
+        if index > 0 and self.heap[index] > self.heap[parent]:
+            self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
+            self.heapify_up(parent)
+
+    # remove maximum element
+    def extract_max(self):
+        if len(self.heap) == 0:
+            return None
+        
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self.heapify_down(0)
+
+        return root
+    
+    #heapify downward
+    def heapify_down(self, index):
+        largest = index
+        left = 2*index + 1
+        right = 2*index + 2
+
+
+        if left < len(self.heap) and self.heap[left] > self.heap[largest]:
+            largest = left
+
+        if right < len(self.heap) and self.heap[right] > self.heap[largest]:
+            largest = right
+
+        if largest != index:
+            self.heap[index], self.heap[largest] = self.heap[largest], self.heap[index]
+            self.heapify_down(largest)
+
+    def print_heap(self):
+        print(self.heap)
+
+
+h = MaxHeap()
+
+h.insert(10)
+h.insert(40)
+h.insert(30)
+h.insert(50)
+
+h.print_heap()
--- DSA 3/Heap/min_heap.py
+++ DSA 3/Heap/min_heap.py
@@ -1,6 +1,55 @@
-# class MinHeap:
-#     def __init__(self):
-#         self.heap = []
+class MinHeap:
+    def __init__(self):
+        self.heap = []
 
-#     def parent(self, i):
-#         return 
+    def insert(self, val):
+        self.heap.append(val)
+        self.heapify_up(len(self.heap)-1)
+
+    def heapify_up(self, index):
+        parent = (index - 1) // 2
+
+        if index > 0 and self.heap[parent] > self.heap[index]:
+            self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
+            self.heapify_up(parent)
+
+    def extract_min(self):
+        if len(self.heap) == 0:
+            return None
+        
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self.heapify_down(0)
+
+        return root
+    
+    def heapify_down(self, index):
+        smallest = index
+        left = 2*index + 1
+        right = 2*index + 2
+
+        if left < len(self.heap) and self.heap[left] < self.heap[smallest] :
+            smallest = left
+
+        if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
+            smallest = right
+
+        if smallest != index:
+            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+            self.heapify_down(smallest)
+
+    def print_heap(self):
+        print(self.heap)
+
+
+h = MinHeap()
+
+h.insert(10)
+h.insert(40)
+h.insert(30)
+h.insert(50)
+
+h.print_heap()
